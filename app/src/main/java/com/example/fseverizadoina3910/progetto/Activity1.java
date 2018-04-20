c4d4a89c142f7d09e810572351d663c9c45cf37f
FSEVERI\zadoina3910 <maxim.zadoina@gmail.com>
2018-04-20 13:45:03 +0200

    comited

--- app/src/main/java/com/example/fseverizadoina3910/progetto/Activity1.java
+++ app/src/main/java/com/example/fseverizadoina3910/progetto/Activity1.java
@@ -9,7 +9,7 @@ public class Activity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
-        ///////////////
+        ///////////////comittt
         ///////////prova3hghgh
     }
 }
